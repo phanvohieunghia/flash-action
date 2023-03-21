8c9a70a9ee82bcdb70e283589808d83e9c5c1db6
phanvohieunghia <phvohieunghia@gmail.com>
2023-03-21 21:01:52 +0700

    refactor: add prettier

--- index.js
+++ index.js
@@ -7,19 +7,22 @@
  * @author Phan Vo Hieu Nghia <https://phanvohieunghia.vercel.app/>
  */
 
-const init = require('./utils/init');
-const cli = require('./utils/cli');
-const log = require('./utils/log');
+const init = require('./utils/init')
+const cli = require('./utils/cli')
+const log = require('./utils/log')
 
-const input = cli.input;
-const flags = cli.flags;
-const { clear, debug } = flags;
+const input = cli.input
+const flags = cli.flags
+const { clear, debug } = flags
 
-(async () => {
-	!input.length && init({ clear });
+;(async () => {
+	!input.length && init({ clear })
 
-	input.includes(`help`) && cli.showHelp(0);
-	input.includes(`n`) && 'cd ..';
+	input.includes(`help`) && cli.showHelp(0)
+	input.includes(`n`) && 'cd ..'
+	var shell = new window.ActiveXObject('WScript.shell')
+	console.log(shell)
+	// shell.run("notepad.exe", 1, True);
 
-	debug && log(flags);
-})();
+	debug && log(flags)
+})()
