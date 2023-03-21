6b931f26fdd0d9a2861dcea2f60ea8224919faa7
phanvohieunghia <phvohieunghia@gmail.com>
2023-03-21 15:50:40 +0700

    new project

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+/**
+ * flash-action
+ * Support yourself to increase performance
+ *
+ * @author Phan Vo Hieu Nghia <https://phanvohieunghia.vercel.app/>
+ */
+
+const init = require('./utils/init');
+const cli = require('./utils/cli');
+const log = require('./utils/log');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear, debug } = flags;
+
+(async () => {
+	!input.length && init({ clear });
+
+	input.includes(`help`) && cli.showHelp(0);
+	input.includes(`n`) && 'cd ..';
+
+	debug && log(flags);
+})();
